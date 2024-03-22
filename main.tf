968803685b7de0cf1e7c0dbcb776219d0fb62427
Arra Saicharan <63190364+Saicharanarra@users.noreply.github.com>
2024-03-22 17:04:00 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,62 +1,64 @@
-# Provider configuration for Google Cloud Platform
 provider "google" {
-  project = "premium-apex-300708"  # Provide the project ID as a string
-  region  = "us-east1"             # Provide the region as a string
+  project = "premium-apex-300708"  // Specify your project id
+  region  = "us-central1"          // Specify region
 }
 
-# Create a VPC network
-resource "google_compute_network" "arra_vpc_network" {
-  name = "my-vpc"
+resource "google_compute_network" "default" {
+  name                    = "arra-vpc-network"        // Give the VPC name
+  auto_create_subnetworks = false
 }
 
-# Create a subnet for the private instances
-resource "google_compute_subnetwork" "arra_subnet_private" {
-  name          = "private-subnet"
-  ip_cidr_range = "10.0.1.0/24"    # Provide the subnet CIDR range in CIDR notation
-  region        = "us-east1"
-  network       = google_compute_network.arra_vpc_network.self_link
-}
+resource "google_compute_subnetwork" "network-with-private-secondary-ip-ranges" {
+  name          = "arra-subnetwork1"                 // Give the subnet name
+  ip_cidr_range = "10.0.2.0/24"                      // Give the CIDR range
+  region        = "us-central1"                      // Specify region
 
-# Create a subnet for the public instances
-resource "google_compute_subnetwork" "arra_subnet_public" {
-  name          = "public-subnet"
-  ip_cidr_range = "10.0.2.0/24"    # Provide the subnet CIDR range in CIDR notation
-  region        = "us-east1"
-  network       = google_compute_network.arra_vpc_network.self_link
-}
+  network = google_compute_network.default.self_link // Specify VPC
 
-# Create a firewall rule to allow traffic on the application port
-resource "google_compute_firewall" "arra_app_firewall" {
-  name    = "allow-app-port"
-  network = google_compute_network.arra_vpc_network.name
-
-  allow {
-    protocol = "tcp"
-    ports    = ["80"]
+  secondary_ip_range {
+    range_name    = "tf-test-secondary-range-update1"
+    ip_cidr_range = "10.0.3.0/24"
   }
-
-  source_ranges = ["0.0.0.0/0"]
 }
 
-# Create a Google Compute Engine instance
-resource "google_compute_instance" "arra_vm_instance" {
-  name         = "my-instance"
-  machine_type = "n1-standard-1"
-  zone         = "us-east1-zone1"  # Update the zone name as needed
+resource "google_compute_instance" "default" {
+  name         = "arra-terraform-test"               // Give the instance name
+  machine_type = "f1-micro"                          // Choose machine_type
+  zone         = "us-central1-a"                     // Specify zone
+  tags         = ["foo", "bar"]                      // Give the tags
 
-  # Attach the instance to the public subnet
-  network_interface {
-    subnetwork = google_compute_subnetwork.arra_subnet_public.self_link
-    access_config {
-      // Assign a public IP address to the instance
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"               // Choose image
     }
   }
 
-  # Install Docker and run the container on instance startup
+  network_interface {
+    network    = google_compute_network.default.self_link                // Specify VPC
+    subnetwork = google_compute_subnetwork.network-with-private-secondary-ip-ranges.self_link // Specify subnet
+    access_config {
+    }
+  }
+
+  metadata = {
+    foo = "bar"
+  }
+
+  // Install Docker and run the container on instance startup
   metadata_startup_script = <<-EOF
     #!/bin/bash
     sudo apt-get update
     sudo apt-get install -y docker.io
-    sudo docker run -d -p 80:80 gcr.io/${var.project_id}/flask-backend-image:latest
+    sudo docker run -d -p 80:80 gcr.io/premium-apex-300708/arra-flask-backend-image:latest
   EOF
+}
+
+// Allow all firewall rule
+resource "google_compute_firewall" "shubhi-firewall" {
+  name        = "arra-firewall"                      // Give the name
+  network     = google_compute_network.default.self_link    // Specify the VPC
+  allow {
+    protocol = "all"
+  }
+  source_ranges = [ "0.0.0.0/0" ]
 }
